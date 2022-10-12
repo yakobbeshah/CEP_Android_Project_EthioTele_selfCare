0ab15fae0ba24f6ec5488726621e510f90ef286f
yakobbeshah <bashayakob@gimal.com>
2022-10-12 06:07:13 -0700

    Project is started

---
+++ app/src/main/java/com/codechat/beqelalutele/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codechat.beqelalutele;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
