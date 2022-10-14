12818d48d9c25bef25e91098984c50ef5ac5cef3
Abualiy <akremmuktar332@gmail.com>
2022-10-14 14:44:30 +0300

    Splash Screen By Ak

--- app/src/main/java/com/codechat/beqelalutele/MainActivity.java
+++ app/src/main/java/com/codechat/beqelalutele/MainActivity.java
@@ -2,13 +2,30 @@ package com.codechat.beqelalutele;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
