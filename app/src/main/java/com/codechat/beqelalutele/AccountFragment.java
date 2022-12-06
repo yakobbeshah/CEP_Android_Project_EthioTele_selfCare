a16b7abd82a9ab6554fd4cc9b1bebeb8c0d2f730
Abualiy <akremmuktar332@gmail.com>
2022-12-07 00:07:18 +0300

    backendAccount

--- app/src/main/java/com/codechat/beqelalutele/AccountFragment.java
+++ app/src/main/java/com/codechat/beqelalutele/AccountFragment.java
@@ -1,64 +1,113 @@
 package com.codechat.beqelalutele;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AccountFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final int REQUEST_CALL = 1;
+    Button balance, recharge, transfer, callBack;
+    EditText cardnumber, tranferphone, transferamount, callphone;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        balance = v.findViewById(R.id.ask);
+        recharge = v.findViewById(R.id.recharge);
+        transfer = v.findViewById(R.id.transfer);
+        callBack = v.findViewById(R.id.call_back);
+        cardnumber = v.findViewById(R.id.card_number);
+        tranferphone = v.findViewById(R.id.transfer_number);
+        transferamount = v.findViewById(R.id.transfer_ammount);
+        callphone = v.findViewById(R.id.call_phone);
+
+        balance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                balanceAsk();
+            }
+        });
+        recharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rechargeAccount();
+            }
+        });
+        transfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tranferBallence();
+            }
+        });
+        callBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callMeBack();
+            }
+        });
+
+        return v;
     }
+    public void balanceAsk() {
+        String number = "*804";
+        String dial = "tel:" + number;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(Uri.parse(String.valueOf(Uri.parse(dial)))+Uri.encode("#")));
+        startActivity(intent);
+    }
+    public void rechargeAccount() {
+        String rechargeNumber = cardnumber.getText().toString();
+        String number = "*805*"+rechargeNumber;
+        String dial = "tel:" + number;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(Uri.parse(String.valueOf(Uri.parse(dial)))+Uri.encode("#")));
+        startActivity(intent);
+        cardnumber.setText("");
+    }
+    public void tranferBallence() {
+        String transferNumber = tranferphone.getText().toString();
+        String transferAmmount = transferamount.getText().toString();
+        String number = "*806*"+transferNumber+"*"+transferAmmount;
+        String dial = "tel:" + number;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(Uri.parse(String.valueOf(Uri.parse(dial)))+Uri.encode("#")));
+        startActivity(intent);
+        tranferphone.setText("");
+        transferamount.setText("");
+    }
+    public void callMeBack() {
+        String callPhone= callphone.getText().toString();
+        String number = "*807*"+callPhone;
+        String dial = "tel:" + number;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(Uri.parse(String.valueOf(Uri.parse(dial)))+Uri.encode("#")));
+        startActivity(intent);
+        callphone.setText("");
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                balanceAsk();
+                rechargeAccount();
+                tranferBallence();
+                callMeBack();
+            }
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/codechat/beqelalutele/HomeFragment.java
+++ app/src/main/java/com/codechat/beqelalutele/HomeFragment.java
@@ -1,70 +1,76 @@
 package com.codechat.beqelalutele;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    TextView account, packageFrom, credit, aboutUs;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        account = v.findViewById(R.id.accountFrom);
+        packageFrom = v.findViewById(R.id.pachageFrom);
+        credit = v.findViewById(R.id.creditFrom);
+        aboutUs = v.findViewById(R.id.aboutusFrom);
+
+
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new AccountFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+        packageFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new PackageFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+
+            }
+        });
+        credit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new CreditFragemnt());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AboutActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+
     }
+
+
 }
